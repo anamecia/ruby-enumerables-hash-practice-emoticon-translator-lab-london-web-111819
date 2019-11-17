2f7f5c9e35a4b24a4e3a37aacbe077e4302c93a6
Ana Robalo <anameciarobalo@gmail.com>
2019-11-17 21:45:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,19 +5,15 @@ require 'pp'
 def load_library(file_path)
   # code goes here
   emoticons = YAML.load_file(file_path)
-  get_meaning = []
-  emoticons.each_key{|key| get_meaning << key}
+  
+  
 end
 
 'lib/emoticons.yml'
 
 def get_japanese_emoticon (yaml_file_path, emoticon)
   # code goes here
-  load_library(yaml_file_path).each do |key,value|
-    if value[0] == emoticon
-      return value[1]
-    end
-    return 'Sorry'
+  
   end
   
    
