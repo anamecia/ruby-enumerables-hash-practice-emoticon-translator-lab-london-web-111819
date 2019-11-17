5796b607421c4220f6ff11ee267b32d1c5aa7d45
Ana Robalo <anameciarobalo@gmail.com>
2019-11-17 21:24:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,15 @@
 # require modules here
+require "yaml"
+require 'pp'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  get_meaning = []
+  emoticons.each_key{|key| get_meaning << key}
 end
+
+'lib/emoticons.yml'
 
 def get_japanese_emoticon
   # code goes here
